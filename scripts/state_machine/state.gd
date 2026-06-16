556b470ea096d4d0a588115bd264fb3f4381f23b
iamai <iamai@iamais-MacBook-Pro.local>
2026-06-16 18:50:12 +0500

    update state machine

--- scripts/state_machine/state.gd
+++ scripts/state_machine/state.gd
@@ -2,6 +2,7 @@ class_name State
 extends Node
 
 var actor: Actor
+var state_machine: StateMachine
 
 func enter() -> void:
 	pass
@@ -14,3 +15,9 @@ func physics_update(delta: float) -> void:
 
 func update(delta: float) -> void:
 	pass
+
+func handle_input(_event: InputEvent) -> void:
+	pass
+	
+func change_state(state_name: StringName) -> void:
+	state_machine.change_state(state_name)
--- scripts/state_machine/state_machine.gd
+++ scripts/state_machine/state_machine.gd
@@ -1,28 +1,68 @@
 class_name StateMachine
 extends Node
 
-signal state_changed(old_state: State, new_state: State)
-
+var states: Dictionary = {}
+var actor: Actor
 var current_state: State
 var previous_state: State
 
-func change_state(new_state: State) -> void:
-	if not new_state or current_state == new_state:
+func initialize(owner: Actor) -> void:
+	actor = owner
+
+	for child in get_children():
+
+		if child is State:
+			states[child.name] = child
+
+			child.actor = actor
+			child.state_machine = self
+			
+func setup_states() -> void:
+
+	for state in states.values():
+
+		if state.has_method("setup"):
+			state.setup()
+			
+func start(initial_state_name: StringName) -> void:
+
+	if not states.has(initial_state_name):
+		push_error("State not found: " + initial_state_name)
 		return
-	
-	if current_state:
-		current_state.exit()
-	
-	previous_state = current_state
-	current_state = new_state
+
+	current_state = states[initial_state_name]
 	current_state.enter()
 	
-	state_changed.emit(previous_state, current_state)
-
 func update(delta: float) -> void:
+
 	if current_state:
 		current_state.update(delta)
-
+		
 func physics_update(delta: float) -> void:
+
 	if current_state:
 		current_state.physics_update(delta)
+		
+func handle_input(event: InputEvent) -> void:
+
+	if current_state:
+		current_state.handle_input(event)
+		
+func change_state(new_state_name: StringName) -> void:
+
+	if not states.has(new_state_name):
+		push_error("State not found: " + new_state_name)
+		return
+
+	var new_state: State = states[new_state_name]
+
+	if new_state == current_state:
+		return
+
+	previous_state = current_state
+
+	if current_state:
+		current_state.exit()
+
+	current_state = new_state
+	current_state.enter()
