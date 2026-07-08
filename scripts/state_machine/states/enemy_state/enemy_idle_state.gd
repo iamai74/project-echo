e06fa5a971476a02c4a972309c0870e18ad434f9
iamai <iamai@iamais-MacBook-Pro.local>
2026-07-08 17:07:19 +0500

    fix test knockback_force

--- scripts/state_machine/states/enemy_state/enemy_idle_state.gd
+++ scripts/state_machine/states/enemy_state/enemy_idle_state.gd
@@ -2,6 +2,7 @@ class_name EnemyIdleState
 extends EnemyState
 
 func physics_update_state(_delta: float) -> void:
+	actor.velocity.x = 0
 	if not actor.is_on_floor():
 		change_state("Fall")
 		return
